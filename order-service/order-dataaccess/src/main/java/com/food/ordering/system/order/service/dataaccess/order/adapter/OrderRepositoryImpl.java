bdb83d612fa1ad083bd2e79045e7c4a381fba2c3
selbokhari <sohaybelb2016@gmail.com>
2024-02-29 16:42:28 +0100

    commit initial

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/adapter/OrderRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.food.ordering.system.order.service.dataaccess.order.adapter;
+
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.order.service.dataaccess.order.mapper.OrderDataAccessMapper;
+import com.food.ordering.system.order.service.dataaccess.order.repository.OrderJpaRepository;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Component
+public class OrderRepositoryImpl implements OrderRepository {
+
+    private final OrderJpaRepository orderJpaRepository;
+    private final OrderDataAccessMapper orderDataAccessMapper;
+
+    public OrderRepositoryImpl(OrderJpaRepository orderJpaRepository,
+                               OrderDataAccessMapper orderDataAccessMapper) {
+        this.orderJpaRepository = orderJpaRepository;
+        this.orderDataAccessMapper = orderDataAccessMapper;
+    }
+
+    @Override
+    public Order save(Order order) {
+        return orderDataAccessMapper.orderEntityToOrder(orderJpaRepository
+                .save(orderDataAccessMapper.orderToOrderEntity(order)));
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Optional<Order> findById(OrderId orderId) {
+        return orderJpaRepository.findById(orderId.getValue())
+                .map(orderDataAccessMapper::orderEntityToOrder);
+    }
+
+    @Override
+    @Transactional
+    public Optional<Order> findByTrackingId(TrackingId trackingId) {
+        return orderJpaRepository.findByTrackingId(trackingId.getValue())
+                .map(orderDataAccessMapper::orderEntityToOrder);
+    }
+}
---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -0,0 +1,33 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Validated
+@Service
+public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+
+    private final OrderPaymentSaga orderPaymentSaga;
+
+    public PaymentResponseMessageListenerImpl(OrderPaymentSaga orderPaymentSaga) {
+        this.orderPaymentSaga = orderPaymentSaga;
+    }
+
+    @Override
+    public void paymentCompleted(PaymentResponse paymentResponse) {
+        OrderPaidEvent domainEvent = orderPaymentSaga.process(paymentResponse);
+        log.info("Publishing OrderPaidEvent for order id: {}", paymentResponse.getOrderId());
+        domainEvent.fire();
+    }
+
+    @Override
+    public void paymentCancelled(PaymentResponse paymentResponse) {
+        orderPaymentSaga.rollback(paymentResponse);
+        log.info("Order is roll backed with failure messages: {}",paymentResponse.getFailureMessages());
+    }
+}
